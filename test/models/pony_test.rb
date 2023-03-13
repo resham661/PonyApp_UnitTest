09a9295484fbd7ca6df65630801ba5ec0ebb47b0
resham661 <reshamgg12@gmail.com>
2023-03-13 17:55:59 +0530

    initial commit

--- test/models/pony_test.rb
+++ test/models/pony_test.rb
@@ -4,22 +4,22 @@ class PonyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  fixures :ponies
-  def test_pony
-    pony = Pony.new :name => ponies(:one).name,
-                        :profesion => pony(:one).profesion
-    assert pony.save
-  end
+  # fixures :ponies
+  # def test_pony
+  #   pony = Pony.new :name => ponies(:one).name,
+  #                       :profesion => pony(:one).profesion
+  #   assert pony.save
+  # end
 
   test 'should not save pony without name' do
     pony = Pony.new
     assert_not pony.save, "Saved the pony without a name"
   end
 
-  test 'should save pony with name' do
-    pony = Pony.new
-    assert pony.save, "Saved the pony with a name"
-  end
+  # test 'should save pony with name' do
+  #   pony = Pony.new
+  #   assert pony.save, "Saved the pony with a name"
+  # end
   # test "has a name" do
   #   pony = Pony.new(
   #     name: '',
